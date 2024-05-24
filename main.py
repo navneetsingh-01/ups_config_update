3e8620a17db4f8675caffe6a3ac1b7df3f79068a
Navneet Singh <singhnavneet@salesforce.com>
2024-05-24 17:26:28 +0530

    some changes

--- main.py
+++ main.py
@@ -34,12 +34,12 @@ try:
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
             client.connect(ip, username=username,
-                           password=password1, timeout=20)
+                           password=password1, timeout=10)
         except Exception as e:
             print("Authentication failed, testing another password")
             try:
                 client.connect(ip, username=username,
-                               password=password2, timeout=20)
+                               password=password2, timeout=10)
             except Exception as e:
                 print("Something went wrong: " + str(e) + "\n\n")
                 continue
@@ -49,47 +49,45 @@ try:
 
         print("\n\n########## Configuring device: " + host)
         print(host, ip)
-        print("Done")
-        # # Disable FTP
-        # shell.send(bytes("ftp -S disable\n", 'ascii'))
-        # result = shell.recv(65535).decode('ascii')
-        # print("FTP Disabled")
-        # # Disable HTTP
-        # shell.send(bytes("web -h disable\n", 'ascii'))
-        # result = shell.recv(65535).decode('ascii')
-        # print("HTTP Disabled")
-        # # Enable SSH
-        # shell.send(bytes("console -s\n", 'ascii'))
-        # result = shell.recv(65535).decode('ascii')
-        # print("SSH Enabled")
-        # # Enable HTTPS
-        # shell.send(bytes("web -s enable\n", 'ascii'))
-        # result = shell.recv(65535).decode('ascii')
-        # print("HTTPS Enabled")
+        # Disable FTP
+        shell.send(bytes("ftp -S disable\n", 'ascii'))
+        result = shell.recv(65535).decode('ascii')
+        print("FTP Disabled")
+        # Disable HTTP
+        shell.send(bytes("web -h disable\n", 'ascii'))
+        result = shell.recv(65535).decode('ascii')
+        print("HTTP Disabled")
+        # Enable SSH
+        shell.send(bytes("console -s\n", 'ascii'))
+        result = shell.recv(65535).decode('ascii')
+        print("SSH Enabled")
+        # Enable HTTPS
+        shell.send(bytes("web -s enable\n", 'ascii'))
+        result = shell.recv(65535).decode('ascii')
+        print("HTTPS Enabled")
 
-        # # Primary NTP Server
-        # shell.send(bytes("ntp -p ntp.internal.salesforce.com\n", 'ascii'))
-        # result = shell.recv(65535).decode('ascii')
-        # print("Primary NTP Server configured")
-        # # Secondary NTP Server
-        # shell.send(bytes("ntp -s phx-ntp.internal.salesforce.com\n", 'ascii'))
-        # result = shell.recv(65535).decode('ascii')
-        # print("Secondary NTP Server configured")
+        # Primary NTP Server
+        shell.send(bytes("ntp -p ntp.internal.salesforce.com\n", 'ascii'))
+        result = shell.recv(65535).decode('ascii')
+        print("Primary NTP Server configured")
+        # Secondary NTP Server
+        shell.send(bytes("ntp -s phx-ntp.internal.salesforce.com\n", 'ascii'))
+        result = shell.recv(65535).decode('ascii')
+        print("Secondary NTP Server configured")
 
-        # # Timezone by default GMT - Same time is displayed in UTC
+        # Timezone by default GMT - Same time is displayed in UTC
 
-        # # Add an admin user
-        # shell.send(bytes("user -n admin -pw Ups#123! -pe Administrator -e enable\n", 'ascii'))
-        # result = shell.recv(65535).decode('ascii')
-        # print("Admin user created")
+        # Add an admin user
+        shell.send(bytes("user -n admin -pw Ups#123! -pe Administrator -e enable\n", 'ascii'))
+        result = shell.recv(65535).decode('ascii')
+        print("Admin user created")
 
-        # # Reboot
-        # shell.send(bytes("reboot\n", 'ascii'))
-        # result = shell.recv(65535).decode('ascii')
-        # shell.send(bytes("YES\n", 'ascii'))
-        # result = shell.recv(65535).decode('ascii')
-        # print("Rebooted successfully")
-
+        # Reboot
+        shell.send(bytes("reboot\n", 'ascii'))
+        result = shell.recv(65535).decode('ascii')
+        shell.send(bytes("YES\n", 'ascii'))
+        result = shell.recv(65535).decode('ascii')
+        print("Rebooted successfully\n######################")
         client.close()
 except Exception as e:
     print("Something went wrong: " + str(e))
