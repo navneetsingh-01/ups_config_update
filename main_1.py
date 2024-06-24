5ceeb0b5227d56b9f6ddbd875026b0b8111c7c09
Navneet Singh <singhnavneet@salesforce.com>
2024-06-24 12:38:37 +0530

    some changes

--- main_1.py
+++ main_1.py
@@ -1,50 +1,52 @@
-import csv
-from ups import UPSConfig
+# import csv
+# from ups import UPSConfig
 
 
-def read_csv(file_path):
-    with open(file_path, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
-        data = [row for row in reader]
-    return data
+# def read_csv(file_path):
+#     with open(file_path, 'r') as csvfile:
+#         reader = csv.DictReader(csvfile)
+#         data = [row for row in reader]
+#     return data
 
-try:
-    csv_file = 'devices_test.csv'
-    data = read_csv(csv_file)
+# try:
+#     csv_file = 'devices_test.csv'
+#     data = read_csv(csv_file)
 
-    for item in data:
-        host = item["ups_name"]
-        ip = item["ups_ip"]
-        newrelic_ip = item["snmp_ip"]
-        username = "apc"
-        password1 = "P@ss4apc"
-        password2 = "apc"
+#     for item in data:
+#         host = item["ups_name"]
+#         ip = item["ups_ip"]
+#         newrelic_ip = item["snmp_ip"]
+#         username = "apc"
+#         password1 = "P@ss4apc"
+#         password2 = "apc"
 
-        if host is None or ip is None:
-            continue
+#         if host is None or ip is None:
+#             continue
 
-        print("\n\n########## Configuring device: " + host + " - " + ip)
-        try:
-            config = UPSConfig(ip, username, [password1, password2])
-        except Exception as e:
-            print("Unable to connect: " + str(e))
-            continue
-        configurations = [
-            config.snmp_newrelic,
-            config.snmp_acl_config, 
-            config.reboot
-        ]
-        params_required = ["snmp_newrelic"]
-        for ups_config in configurations:
-            try:
-                if ups_config.__name__ in params_required:
-                    ups_config(newrelic_ip)
-                else:
-                    ups_config()
-            except Exception as e:
-                print(
-                    f"Something went wrong while executing {ups_config.__name__}: " + str(e))
+#         print("\n\n########## Configuring device: " + host + " - " + ip)
+#         try:
+#             config = UPSConfig(ip, username, [password1, password2])
+#         except Exception as e:
+#             print("Unable to connect: " + str(e))
+#             continue
+#         configurations = [
+#             config.snmp_newrelic,
+#             config.snmp_acl_config,
+#             config.reboot
+#         ]
+#         params_required = ["snmp_newrelic"]
+#         for ups_config in configurations:
+#             try:
+#                 if ups_config.__name__ in params_required:
+#                     ups_config(newrelic_ip)
+#                 else:
+#                     ups_config()
+#             except Exception as e:
+#                 print(
+#                     f"Something went wrong while executing {ups_config.__name__}: " + str(e))
 
-        config.close_connection()
-except Exception as e:
-    print("Something went wrong: " + str(e))
+#         config.close_connection()
+# except Exception as e:
+#     print("Something went wrong: " + str(e))
+import os
+print(os.getenv("SU_USERNAME"))
