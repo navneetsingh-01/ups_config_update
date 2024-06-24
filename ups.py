bbd644dd2c681459598fbbd83be28dcd6336df4a
Navneet Singh <singhnavneet@salesforce.com>
2024-06-24 12:19:14 +0530

    some changes

--- ups.py
+++ ups.py
@@ -140,10 +140,16 @@ class UPSConfig:
     def snmp_acl_config(self):
         self.shell.send(bytes("snmpv3 -ac1 enable\n", 'ascii'))
         result = self.shell.recv(65535).decode('ascii')
-        print("SNMP ACL enabled")
+        if "Success" in result:
+            print("SNMP ACL enabled")
+        else:
+            print("Could not enable SNMP ACL")
 
     def snmp_newrelic(self, newrelic_ip):
         cmd = f"snmpv3 -n1 {newrelic_ip}\n"
         self.shell.send(bytes(cmd, 'ascii'))
         result = self.shell.recv(65535).decode('ascii')
-        print("SNMP ACL enabled")
+        if "Success" in result:
+            print("NewRelic IP configured")
+        else:
+            print("Unable to configure NewRelic IP")
