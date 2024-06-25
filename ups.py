805349bfc666248c05ee9eaf87331d2e742c62d9
Navneet Singh <singhnavneet@salesforce.com>
2024-06-25 11:48:36 +0530

    some changes

--- ups.py
+++ ups.py
@@ -170,15 +170,13 @@ class UPSConfig:
     def snmp_access_ips(self, newrelic_ip):
         cmd = f"snmpv3 -n1 {newrelic_ip}\n"
         self.shell.send(bytes(cmd, 'ascii'))
-        result = ""
         buffer = 20
-        while buffer>0:
-            if self.shell.recv_ready():
-                result += self.shell.recv(10000000).decode('ascii')
+        while not self.shell.recv_ready() and buffer:
+            print("NOT READY - recv_ready: " +
+                    str(self.shell.recv_ready()) + "\n")
             time.sleep(1)
-            if self.shell.exit_status_ready():
-                break
-            buffer-=1
+            buffer -= 1
+        result = self.shell.recv(10000000).decode('ascii')
         print(result)
         print("NewRelic IP configured")
 
@@ -187,28 +185,24 @@ class UPSConfig:
 
         cmd = f"snmpv3 -n2 {eco_ip1}\n"
         self.shell.send(bytes(cmd, 'ascii'))
-        result = ""
         buffer = 20
-        while buffer>0:
-            if self.shell.recv_ready():
-                result += self.shell.recv(10000000).decode('ascii')
+        while not self.shell.recv_ready() and buffer:
+            print("NOT READY - recv_ready: " +
+                    str(self.shell.recv_ready()) + "\n")
             time.sleep(1)
-            if self.shell.exit_status_ready():
-                break
-            buffer-=1
+            buffer -= 1
+        result = self.shell.recv(10000000).decode('ascii')
         print(result)
         print("N2 IP configured")
 
         cmd = f"snmpv3 -n3 {eco_ip2}\n"
         self.shell.send(bytes(cmd, 'ascii'))
-        result = ""
         buffer = 20
-        while buffer>0:
-            if self.shell.recv_ready():
-                result += self.shell.recv(10000000).decode('ascii')
+        while not self.shell.recv_ready() and buffer:
+            print("NOT READY - recv_ready: " +
+                    str(self.shell.recv_ready()) + "\n")
             time.sleep(1)
-            if self.shell.exit_status_ready():
-                break
-            buffer-=1
+            buffer -= 1
+        result = self.shell.recv(10000000).decode('ascii')
         print(result)
         print("N3 IP configured")
