a88f2c4cd742d56d478d8b69e0cbc3e59677b08b
Navneet Singh <singhnavneet@salesforce.com>
2024-06-25 11:27:48 +0530

    some changes

--- ups.py
+++ ups.py
@@ -170,24 +170,30 @@ class UPSConfig:
     def snmp_access_ips(self, newrelic_ip):
         cmd = f"snmpv3 -n1 {newrelic_ip}\n"
         self.shell.send(bytes(cmd, 'ascii'))
-        time.sleep(1)
-        result = self.shell.recv(10000000).decode('ascii')
-        print("NewRelic IP configured")
-        print(result)
+        time.sleep(2)
+        if self.shell.recv_ready():
+            result = self.shell.recv(10000000).decode('ascii')
+            print(result)
+            print("NewRelic IP configured")
+            
 
         eco_ip1 = "10.15.96.101"
         eco_ip2 = "10.192.100.37"
 
         cmd = f"snmpv3 -n2 {eco_ip1}\n"
         self.shell.send(bytes(cmd, 'ascii'))
-        time.sleep(1)
-        result = self.shell.recv(10000000).decode('ascii')
-        print("N2 IP configured")
-        print(result)
+        time.sleep(2)
+        if self.shell.recv_ready():
+            result = self.shell.recv(10000000).decode('ascii')
+            print(result)
+            print("N2 IP configured")
+            
 
         cmd = f"snmpv3 -n3 {eco_ip2}\n"
         self.shell.send(bytes(cmd, 'ascii'))
-        time.sleep(1)
-        result = self.shell.recv(10000000).decode('ascii')
-        print("N3 IP configured")
-        print(result)
+        time.sleep(2)
+        if self.shell.recv_ready():
+            result = self.shell.recv(10000000).decode('ascii')
+            print(result)
+            print("N3 IP configured")
+            
